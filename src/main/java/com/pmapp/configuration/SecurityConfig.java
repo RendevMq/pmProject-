10392bb8bb21157a710a39cd69619afa9a7ed83c
RendevMq <rensystem73@gmail.com>
2024-11-05 23:31:54 -0500

    crontolador

--- src/main/java/com/pmapp/configuration/SecurityConfig.java
+++ src/main/java/com/pmapp/configuration/SecurityConfig.java
@@ -26,7 +26,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity // Habilitamos la seguridad basada en anotaciones
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
